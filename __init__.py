c67fc3ca6882330282de62c7dd538b95d270b5fe
iamjustkoi <iamjustkoi@gmail.com>
2022-06-13 14:44:04 -0400

    Implemented review time methods with UI updates.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,38 @@
+# import aqt.preferences
 from PyQt5.QtWidgets import QAction
 # from anki import cards
 from aqt import mw, deckbrowser
+import datetime
+
+delta_time = datetime.timedelta
+date_time = datetime.datetime
+
+
+class Text:
+    TOTAL = "Total"
+    PAST_WEEK = "Past Week"
+    HRS = "hrs"
+    MIN = "min"
+
+
+class Day:
+    MON = 0
+    TUE = 1
+    WED = 2
+    THU = 3
+    FRI = 4
+    SAT = 5
+    SUN = 6
+
+
+# from pytz import timezone
+# from aqt import preferences
+# mw.col.conf
+# mw.col.crt
+# TODO: get actual preference values
+week_start_day = Day.SUN
+offset_hour = 6
+
 
 html_time = """    
         <style>
@@ -18,14 +50,14 @@ html_time = """
                 font-weight: normal;
             }}
         </style>
-        <table width="70%">
+        <table width="70%%" id="time_table">
             <tr>
-                <th class="time-studied-header">Total Hours</th>
-                <th class="time-studied-header">Past Week</th>
+                <th class="time-studied-header">{total_label}</th>
+                <th class="time-studied-header">{range_label}</th>
             </tr>
             <tr>
-                <td class="time-studied-row">{}</td>
-                <td class="time-studied-row">{}</td>
+                <td class="time-studied-row">{total_value} {total_unit}</td>
+                <td class="time-studied-row">{range_value} {range_unit}</td>
             </tr>
         </table>
 """
@@ -37,59 +69,102 @@ def build_hooks():
 
 
 def on_deck_browser_will_render_content(deck_browser: deckbrowser.DeckBrowser, content: deckbrowser.DeckBrowserContent):
-    total, ranged = get_deck_time_spent()
-    content.stats += html_time.format(total, ranged)
+    total, ranged = get_review_times()
+
+    total_val = round(total, 2)
+    total_unit = Text.HRS
+
+    if ranged > 1:
+        ranged_value = round(ranged, 2)
+        range_unit = Text.HRS
+    else:
+        ranged_value = round(ranged * 60, 2)
+        range_unit = Text.MIN
+
+    content.stats += html_time.format(total_label=Text.TOTAL, range_label=Text.PAST_WEEK,
+                                      total_value=total_val, range_value=ranged_value,
+                                      total_unit=total_unit, range_unit=range_unit)
 
 
-def get_deck_time_spent() -> (float, float):
-    if mw.state == "deckBrowser":
+def get_review_times() -> (float, float):
+    if mw.state == "overview":
         dids = [str(i) for i in mw.col.decks.deck_and_child_ids(mw.col.decks.current().get("id"))]
     else:
         dids = mw.col.decks.allIds()
 
     dids_as_args = "(" + ", ".join(dids) + ")"
-    cids = mw.col.db.all(f'''SELECT id FROM cards WHERE did in {dids_as_args}\n''')
+    cids_cmd = f'SELECT id FROM cards WHERE did in {dids_as_args}\n'
+    cids = mw.col.db.all(cids_cmd)
+
     formatted_cids = "(" + (str(cids).replace("[", "").replace("]", "")) + ")"
+    revlog_cmd = f'SELECT id, time FROM revlog WHERE cid in {formatted_cids}'
+    rev_log = mw.col.db.all(revlog_cmd)
 
-    cmd_all = f'''SELECT cid, time FROM revlog WHERE cid in {formatted_cids}'''
-    rev_stats_all = mw.col.db.all(cmd_all)
+    days_from_last_start = date_time.today().weekday() - week_start_day + 7
+    date_of_last_start = date_time.today() - delta_time(days=days_from_last_start)
+    filtered_revlog = filter_revlog(rev_log, after=date_of_last_start)
 
-    # cmd_ranged = f'''SELECT cid, time FROM revlog WHERE day >= '''
-    # rev_stats_range = mw.col.db.all(cmd_ranged)
+    all_rev_times_ms = [log[1] for log in rev_log[0:]]
+    filtered_rev_times_ms = [log[1] for log in filtered_revlog[0:]]
 
-    # rev_ids_all = [i[0] for i in rev_stats_all[0:]]
-    rev_times_all_ms = [i[1] for i in rev_stats_all[0:]]
+    return sum(all_rev_times_ms) / 1000 / 60 / 60, sum(filtered_rev_times_ms) / 1000 / 60 / 60
 
-    return round(sum(rev_times_all_ms) / 1000 / 60, 2), 0.0
 
-def get_time_action():
-    if mw.state == "deckBrowser":
+def offset_date_hour(date: date_time, offset: int = offset_hour):
+    return date - delta_time(hours=offset)
+
+
+def filter_revlog(
+        rev_logs: [[int, int]], days_ago: int = None, after: date_time = None
+) -> [[int, int]]:
+
+    print(f'check against: {offset_date_hour(after)}')
+
+    filtered_logs = []
+    for log in rev_logs[0:]:
+
+        log_epoch_seconds = log[0] / 1000
+        log_date = offset_date_hour(date_time.fromtimestamp(log_epoch_seconds))
+
+        if days_ago is not None:
+            log_days_ago = offset_date_hour(date_time.now()) - log_date
+            if log_days_ago.days < days_ago:
+                filtered_logs.append(log)
+
+        elif after is not None:
+            log_days_after = (log_date - offset_date_hour(after)).days
+            if log_days_after >= 0:
+                filtered_logs.append(log)
+
+    return filtered_logs
+
+
+def test_action():
+    if mw.state == "overview":
         dids = [str(i) for i in mw.col.decks.deck_and_child_ids(mw.col.decks.current().get("id"))]
     else:
         dids = mw.col.decks.allIds()
 
     dids_as_args = "(" + ", ".join(dids) + ")"
-    cids = mw.col.db.all(f'''SELECT id FROM cards WHERE did in {dids_as_args}\n''')
+    cids = mw.col.db.all(f'SELECT id FROM cards WHERE did in {dids_as_args}\n')
     formatted_cids = "(" + (str(cids).replace("[", "").replace("]", "")) + ")"
 
-    rev_stats = mw.col.db.all(f'''SELECT cid, time FROM revlog WHERE cid in {formatted_cids}''')
-    rev_ids = [i[0] for i in rev_stats[0:]]
-    rev_times_ms = [i[1] for i in rev_stats[0:]]
+    # Result: [ [ids(time of study), ...], [time(time elapsed), ...] ]
+    rev_logs = mw.col.db.all(f'SELECT id, time FROM revlog WHERE cid in {formatted_cids}')
 
-    print(f"id 1:\n {rev_ids[0]}")
-    # format_id_time = time.struct_time('%Y-%m-%d %H:%M:%S', time.localtime(rev_ids[0]))
+    names = [mw.col.decks.name(i) for i in dids]
+    print(f'dids: {dids_as_args}')
+    print(f'dnames: {names}')
 
-    import datetime
-    format_id_time = datetime.datetime.fromtimestamp(rev_ids[-0] / 1000).strftime('%c')
-    # format_id_time = datetime.datetime.fromtimestamp(1654620763302 / 1000).strftime('%c')
+    filtered_revlog = filter_revlog(rev_logs, days_ago=7)
+    filtered_rev_times_ms = [i[1] for i in filtered_revlog[0:]]
 
-    print(f"today id to time: {format_id_time}")
-    # tooltip(f"total: {len(rev_ids)} time (hr): {sum(rev_times_ms) / 1000 / 60}", period=6000, x_offset=50)
+    print(round(sum(filtered_rev_times_ms) / 1000 / 60 / 60, 2))
 
 
 build_hooks()
-action_stats = QAction("Get Time", mw)
-action_stats.triggered.connect(get_time_action)
+action_stats = QAction("Timelog Test", mw)
+action_stats.triggered.connect(test_action)
 if mw.form.menuTools.actions().__contains__(action_stats):
     mw.form.menuTools.removeAction(action_stats)
 mw.form.menuTools.addAction(action_stats)
