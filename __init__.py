9dbef8118899fb1099b730da7d5d8d8536c2dbc3
iamjustkoi <iamjustkoi@gmail.com>
2022-06-14 15:35:15 -0400

    Added stats to deckbrowser page using new content html formatting.

--- __init__.py
+++ __init__.py
@@ -1,18 +1,22 @@
-# import aqt.preferences
+import aqt.webview
 from PyQt5.QtWidgets import QAction
-# from anki import cards
-from aqt import mw, deckbrowser
+from aqt import mw
+from aqt.deckbrowser import DeckBrowser
+from aqt.overview import Overview
 import datetime
+
+# import aqt.preferences
+# from anki import cards
 
 delta_time = datetime.timedelta
 date_time = datetime.datetime
 
 
 class Text:
-    TOTAL = "Total"
-    PAST_WEEK = "Past Week"
-    HRS = "hrs"
-    MIN = "min"
+    TOTAL = 'Total'
+    PAST_WEEK = 'Past Week'
+    HRS = 'hrs'
+    MIN = 'min'
 
 
 class Day:
@@ -29,12 +33,12 @@ class Day:
 # from aqt import preferences
 # mw.col.conf
 # mw.col.crt
-# TODO: get value from addon config
+# TODO: make addon config val
 week_start_day = Day.SUN
-# TODO: get value from anki preferences
+# TODO: get value from anki profile prefs
 offset_hour = 6
 
-html_time = """    
+stats_html_shell = '''    
         <style>
             .time-studied-header {{
                 text-align: center;
@@ -50,56 +54,42 @@ html_time = """
                 font-weight: normal;
             }}
         </style>
-        <table width="60%%" id="time_table" style="margin: 12px;">
-            <tr>
-                <th class="time-studied-header">{total_label}</th>
-                <th class="time-studied-header">{range_label}</th>
-            </tr>
-            <tr>
-                <td class="time-studied-row">{total_value} {total_unit}</td>
-                <td class="time-studied-row">{range_value} {range_unit}</td>
-            </tr>
-        </table>
-"""
+        <center>
+            <table width="60%%" id="time_table" style="margin: 12px;">
+                <tr>
+                        <th class="time-studied-header">{total_label}</th>
+                    <th class="time-studied-header">{range_label}</th>
+                </tr>
+                <tr>
+                    <td class="time-studied-row">{total_value} {total_unit}</td>
+                    <td class="time-studied-row">{range_value} {range_unit}</td>
+                </tr>
+            </table>
+        </center>
+'''
 
 
 def build_hooks():
     from aqt import gui_hooks
-    gui_hooks.deck_browser_will_render_content.append(on_deck_browser_will_render_content)
+    gui_hooks.webview_will_set_content.append(on_webview_will_set_content)
 
 
-# TODO: add to overview page
-# TODO: add to congrats page
-
-def on_deck_browser_will_render_content(deck_browser: deckbrowser.DeckBrowser, content: deckbrowser.DeckBrowserContent):
-    total, ranged = get_review_times()
-
-    total_val = round(total, 2)
-    total_unit = Text.HRS
-
-    if ranged > 1:
-        ranged_value = round(ranged, 2)
-        range_unit = Text.HRS
-    else:
-        ranged_value = round(ranged * 60, 2)
-        range_unit = Text.MIN
-
-    content.stats += html_time.format(total_label=Text.TOTAL, range_label=Text.PAST_WEEK,
-                                      total_value=total_val, range_value=ranged_value,
-                                      total_unit=total_unit, range_unit=range_unit)
+def on_webview_will_set_content(content: aqt.webview.WebContent, context: object or None):
+    if isinstance(context, (DeckBrowser, Overview)):
+        content.body += stats_html()
 
 
 def get_review_times() -> (float, float):
-    if mw.state == "overview":
-        dids = [str(i) for i in mw.col.decks.deck_and_child_ids(mw.col.decks.current().get("id"))]
+    if mw.state == 'overview':
+        dids = [str(i) for i in mw.col.decks.deck_and_child_ids(mw.col.decks.current().get('id'))]
     else:
         dids = mw.col.decks.allIds()
 
-    dids_as_args = "(" + ", ".join(dids) + ")"
+    dids_as_args = '(' + ', '.join(dids) + ')'
     cids_cmd = f'SELECT id FROM cards WHERE did in {dids_as_args}\n'
     cids = mw.col.db.all(cids_cmd)
 
-    formatted_cids = "(" + (str(cids).replace("[", "").replace("]", "")) + ")"
+    formatted_cids = '(' + (str(cids).replace('[', '').replace(']', '')) + ')'
     revlog_cmd = f'SELECT id, time FROM revlog WHERE cid in {formatted_cids}'
     rev_log = mw.col.db.all(revlog_cmd)
 
@@ -113,29 +103,41 @@ def get_review_times() -> (float, float):
     return sum(all_rev_times_ms) / 1000 / 60 / 60, sum(filtered_rev_times_ms) / 1000 / 60 / 60
 
 
-def offset_date_hour(date: date_time, offset: int = offset_hour):
-    return date - delta_time(hours=offset)
+def stats_html():
+    total, ranged = get_review_times()
+    total_val = round(total, 2) if total > 1 else round(total * 60, 2)
+    total_unit = Text.HRS if total > 1 else Text.MIN
+    range_val = round(ranged, 2) if ranged > 1 else round(ranged * 60, 2)
+    range_unit = Text.HRS if ranged > 1 else Text.MIN
+    return stats_html_shell.format(total_label=Text.TOTAL, range_label=Text.PAST_WEEK,
+                                   total_value=total_val, range_value=range_val,
+                                   total_unit=total_unit, range_unit=range_unit)
+
+
+def offset_date(date: date_time, hours: int = offset_hour):
+    return date - delta_time(hours=hours)
 
 
 def filter_revlog(
-        rev_logs: [[int, int]], days_ago: int = None, after: date_time = None
+        rev_logs: [[int, int]],
+        days_ago: int = None,
+        after: date_time = None
 ) -> [[int, int]]:
-
-    print(f'check against: {offset_date_hour(after)}')
+    print(f'check against: {offset_date(after)}')
 
     filtered_logs = []
     for log in rev_logs[0:]:
 
         log_epoch_seconds = log[0] / 1000
-        log_date = offset_date_hour(date_time.fromtimestamp(log_epoch_seconds))
+        log_date = offset_date(date_time.fromtimestamp(log_epoch_seconds))
 
         if days_ago is not None:
-            log_days_ago = offset_date_hour(date_time.now()) - log_date
+            log_days_ago = offset_date(date_time.now()) - log_date
             if log_days_ago.days < days_ago:
                 filtered_logs.append(log)
 
         elif after is not None:
-            log_days_after = (log_date - offset_date_hour(after)).days
+            log_days_after = (log_date - offset_date(after)).days
             if log_days_after >= 0:
                 filtered_logs.append(log)
 
@@ -143,14 +145,14 @@ def filter_revlog(
 
 
 def test_action():
-    if mw.state == "overview":
-        dids = [str(i) for i in mw.col.decks.deck_and_child_ids(mw.col.decks.current().get("id"))]
+    if mw.state == 'overview':
+        dids = [str(i) for i in mw.col.decks.deck_and_child_ids(mw.col.decks.current().get('id'))]
     else:
         dids = mw.col.decks.allIds()
 
-    dids_as_args = "(" + ", ".join(dids) + ")"
+    dids_as_args = '(' + ', '.join(dids) + ')'
     cids = mw.col.db.all(f'SELECT id FROM cards WHERE did in {dids_as_args}\n')
-    formatted_cids = "(" + (str(cids).replace("[", "").replace("]", "")) + ")"
+    formatted_cids = '(' + (str(cids).replace('[', '').replace(']', '')) + ')'
 
     # Result: [ [ids(time of study), ...], [time(time elapsed), ...] ]
     rev_logs = mw.col.db.all(f'SELECT id, time FROM revlog WHERE cid in {formatted_cids}')
@@ -166,8 +168,9 @@ def test_action():
 
 
 build_hooks()
-action_stats = QAction("Timelog Test", mw)
+action_stats = QAction('Timelog Test', mw)
 action_stats.triggered.connect(test_action)
+# Handle edge cases where toolbar action already exists
 if mw.form.menuTools.actions().__contains__(action_stats):
     mw.form.menuTools.removeAction(action_stats)
 mw.form.menuTools.addAction(action_stats)
