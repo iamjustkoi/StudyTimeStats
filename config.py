97f13c06534355bcfcf0922227f41d6a4b9c564a
iamjustkoi <iamjustkoi@gmail.com>
2022-06-17 13:47:14 -0400

    Created options.ui template.

--- config.py
+++ config.py
@@ -2,12 +2,26 @@ from aqt import AnkiQt
 from .consts import CONFIG_DEFAULTS, CONFIG_WEEK_START
 
 
-class TimeStatsConfigManager:
+class RTConfigManager:
     fields = {CONFIG_WEEK_START}
-    _mw = None
 
     def __init__(self, mw: AnkiQt):
         self._mw = mw
         self.config = mw.col.get_config(__name__, default=CONFIG_DEFAULTS)
-        print(f'Conf name: {__name__}')
         self._mw.col.set_config(__name__, self.config)
+
+        print(f'Config: {__name__}')
+
+    def get(self):
+        config = self._mw.col.conf.get(__name__, {})
+        for field in self.fields:
+            if field not in config:
+                config[field] = CONFIG_DEFAULTS[field]
+        return config
+
+    def set(self, new_conf):
+        conf_manager = self._mw.col.conf
+        if __name__ not in conf_manager:
+            conf_manager[__name__] = {}
+        for field in self.fields:
+            conf_manager[__name__][field] = new_conf[field]
