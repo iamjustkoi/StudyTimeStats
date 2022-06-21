f091ecbabfbc6703524f06ca7b26a35057111ee7
iamjustkoi <iamjustkoi@gmail.com>
2022-06-21 14:32:10 -0400

    Changed some default values and re-added options_dialog.ui to git.

--- options.py
+++ options.py
@@ -1,14 +1,17 @@
 from aqt.qt import QDialog
 from .config import StudyTimeStatsConfig
+from .consts import Config
 from .options_dialog import Ui_OptionsDialog
 
 
 class OptionsDialog(QDialog):
+
     def __init__(self, config: StudyTimeStatsConfig):
         super().__init__()
 
         self.ui = Ui_OptionsDialog()
         self.ui.setupUi(OptionsDialog=self)
 
-
+        # TODO: get config value and apply to dropdown
+        # self.ui.week_start_dropdown.setCurrentIndex(config.get_config().get(Config.WEEK_START))
 
