6d47feeb5dbfa7b4c93f6375a4b2df05f51671a0
iamjustkoi <iamjustkoi@gmail.com>
2022-06-25 11:53:03 -0400

    Moved options.py init methods that don't need updates back to the __init__ call and made the options window no longer modal.

--- options.py
+++ options.py
@@ -6,29 +6,52 @@ from .options_dialog import Ui_OptionsDialog
 from aqt.studydeck import StudyDeck
 
 
+def get_background_color(label: QLabel):
+    stylesheet = label.styleSheet()
+    return stylesheet.replace('QWidget {background-color: ', '').replace('}', '')
+
+
 def set_background_color(label: QLabel, hex_arg: str):
     label.setStyleSheet(f'QWidget {{background-color: {hex_arg}}}')
 
 
-def get_background_color(label: QLabel):
-    stylesheet = label.styleSheet()
-    return stylesheet.replace('QWidget {background-color: ', '').replace('}', '')
+def open_color_dialog(preview: QLabel):
+    color = QColorDialog.getColor(initial=QColor(get_background_color(preview)), options=QColorDialog.ShowAlphaChannel)
+    if color.isValid():
+        set_background_color(preview, color.name(QColor.HexArgb))
 
 
 class TimeStatsOptionsDialog(QDialog):
 
     def __init__(self, conf_manager: TimeStatsConfigManager):
         super().__init__()
-        self.ui = Ui_OptionsDialog()
-        self.ui.setupUi(OptionsDialog=self)
         self.manager = conf_manager
         self.config = conf_manager.config
+        self.ui = Ui_OptionsDialog()
+        self.ui.setupUi(OptionsDialog=self)
 
-        self._load(True)
+        # Exclude Tab Button Actions
+        self.ui.add_button.clicked.connect(self.on_add_clicked)
+        self.ui.remove_button.clicked.connect(self.on_remove_clicked)
 
-    def accept(self) -> None:
-        self._save()
-        self.close()
+        # Restore Defaults Button
+        self.ui.confirm_button_box.button(QDialogButtonBox.RestoreDefaults).clicked.connect(self.on_restore_defaults)
+
+        # Color Select Button
+        self.ui.primary_color_button.clicked.connect(lambda: open_color_dialog(self.ui.primary_color_preview))
+        self.ui.seconday_color_button.clicked.connect(lambda: open_color_dialog(self.ui.secondary_color_preview))
+
+        # Range Button
+        self.ui.range_select_dropdown.activated[int].connect(self.on_range_type_change)
+        self.ui.use_calendar_checkbox.clicked.connect(self.update_calendar_range_extras)
+
+        # Update the position of the custom spinbox to the same position as the calendar checkbox
+        self.ui.appearance_grid_layout.replaceWidget(self.ui.use_calendar_checkbox, self.ui.custom_range_spinbox)
+
+        # Update custom range's max value
+        self.ui.custom_range_spinbox.setMaximum(self.manager.max_range)
+
+        self._load()
 
     def on_range_type_change(self, idx: int):
         if idx == RangeType.CUSTOM:
@@ -41,42 +64,6 @@ class TimeStatsOptionsDialog(QDialog):
             self.ui.appearance_grid_layout.replaceWidget(self.ui.custom_range_spinbox, self.ui.use_calendar_checkbox)
 
         self.update_calendar_range_extras()
-
-    # def update_use_cal_checkbox_label(self):
-
-    def _redraw_cal_checkbox(self):
-        width = self.ui.use_calendar_checkbox.width()
-        height = self.ui.use_calendar_checkbox.height()
-        x = self.ui.use_calendar_checkbox.pos().x()
-        y = self.ui.use_calendar_checkbox.pos().y()
-
-        self.ui.use_calendar_checkbox.setGeometry(QRect(x, y, width, height))
-
-    def update_calendar_range_extras(self):
-        self._redraw_cal_checkbox()
-        dropdown_index = self.ui.range_select_dropdown.currentIndex()
-        if dropdown_index != RangeType.CUSTOM:
-            text_range = {
-                RangeType.WEEK: Text.WEEK,
-                RangeType.TWO_WEEKS: Text.WEEK,
-                RangeType.MONTH: Text.MONTH,
-                RangeType.YEAR: Text.YEAR
-            }
-            self.ui.use_calendar_checkbox.setText(f'{Text.USE_CALENDAR} {text_range[dropdown_index]}')
-
-        using_calendar_range = self.ui.use_calendar_checkbox.isChecked()
-        if (dropdown_index == RangeType.WEEK or dropdown_index == RangeType.TWO_WEEKS) and using_calendar_range:
-            self.ui.week_start_dropdown.show()
-            self.ui.week_start_label.show()
-        else:
-            self.ui.week_start_dropdown.hide()
-            self.ui.week_start_label.hide()
-
-    def on_deck_excluded(self, study_deck: StudyDeck):
-        excluded_deck_name = study_deck.name
-        self.excluded_deck_names += [excluded_deck_name]
-        self.config[Config.EXCLUDED_DIDS] += [self.manager.decks.id(name=excluded_deck_name, create=False)]
-        self.ui.excluded_decks_list.addItem(excluded_deck_name)
 
     def on_add_clicked(self):
         from aqt.qt import QDialogButtonBox
@@ -104,59 +91,11 @@ class TimeStatsOptionsDialog(QDialog):
         self.ui.excluded_decks_list.clear()
         self.ui.excluded_decks_list.addItems(self.excluded_deck_names)
 
-    def on_color_dialog(self, preview: QLabel):
-        color = \
-            QColorDialog.getColor(initial=QColor(get_background_color(preview)), options=QColorDialog.ShowAlphaChannel)
-        if color.isValid():
-            set_background_color(preview, color.name(QColor.HexArgb))
-
-    def _load(self, init=False):
-        ui = self.ui
-
-        if init:
-            # Exclude Tab Button Actions
-            ui.add_button.clicked.connect(self.on_add_clicked)
-            ui.remove_button.clicked.connect(self.on_remove_clicked)
-
-            # Restore Defaults Button Action
-            ui.confirm_button_box.button(QDialogButtonBox.RestoreDefaults).clicked.connect(self.on_restore_defaults)
-
-            # Color Select Button Actions
-            ui.primary_color_button.clicked.connect(lambda: self.on_color_dialog(ui.primary_color_preview))
-            ui.seconday_color_button.clicked.connect(lambda: self.on_color_dialog(ui.secondary_color_preview))
-
-            # update the position of the custom spinbox to the same position as the calendar checkbox
-            self.ui.appearance_grid_layout.replaceWidget(self.ui.use_calendar_checkbox, self.ui.custom_range_spinbox)
-
-            ui.range_select_dropdown.activated[int].connect(self.on_range_type_change)
-            # ui.use_calendar_checkbox.activated[bool].connect(self.update_calendar_range_extras)
-            ui.use_calendar_checkbox.clicked.connect(self.update_calendar_range_extras)
-            ui.custom_range_spinbox.setMaximum(self.manager.max_range)
-
-        ui.week_start_dropdown.setCurrentIndex(self.config[Config.WEEK_START])
-
-        ui.range_select_dropdown.setCurrentIndex(self.config[Config.RANGE_TYPE])
-        self.on_range_type_change(self.ui.range_select_dropdown.currentIndex())
-
-        ui.use_calendar_checkbox.setChecked(self.config[Config.USE_CALENDAR_RANGE])
-        self.update_calendar_range_extras()
-
-        ui.custom_range_spinbox.setValue(self.config[Config.CUSTOM_RANGE])
-        ui.total_line.setText(self.config[Config.CUSTOM_TOTAL_TEXT])
-        ui.ranged_line.setText(self.config[Config.CUSTOM_RANGE_TEXT])
-
-        # Color Pickers
-        set_background_color(ui.primary_color_preview, self.config[Config.PRIMARY_COLOR])
-        set_background_color(ui.secondary_color_preview, self.config[Config.SECONDARY_COLOR])
-
-        ui.browser_checkbox.setChecked(self.config[Config.BROWSER_ENABLED])
-        ui.overview_checkbox.setChecked(self.config[Config.OVERVIEW_ENABLED])
-        ui.congrats_checkbox.setChecked(self.config[Config.CONGRATS_ENABLED])
-
-        # Excluded Decks
-        self.excluded_deck_names = [self.manager.decks.name(i) for i in self.config.get(Config.EXCLUDED_DIDS)]
-        ui.excluded_decks_list.clear()
-        ui.excluded_decks_list.addItems(self.excluded_deck_names)
+    def on_deck_excluded(self, study_deck: StudyDeck):
+        excluded_deck_name = study_deck.name
+        self.excluded_deck_names += [excluded_deck_name]
+        self.config[Config.EXCLUDED_DIDS] += [self.manager.decks.id(name=excluded_deck_name, create=False)]
+        self.ui.excluded_decks_list.addItem(excluded_deck_name)
 
     def on_restore_defaults(self):
         print(f'restore defaults')
@@ -164,6 +103,68 @@ class TimeStatsOptionsDialog(QDialog):
             # load temp defaults
             self.config[field] = Config.DEFAULT_CONFIG[field]
         self._load()
+
+    def accept(self) -> None:
+        self._save()
+        self.close()
+
+    def update_calendar_range_extras(self):
+        self._redraw_cal_checkbox()
+        dropdown_index = self.ui.range_select_dropdown.currentIndex()
+        if dropdown_index != RangeType.CUSTOM:
+            text_range = {
+                RangeType.WEEK: Text.WEEK,
+                RangeType.TWO_WEEKS: Text.WEEK,
+                RangeType.MONTH: Text.MONTH,
+                RangeType.YEAR: Text.YEAR
+            }
+            self.ui.use_calendar_checkbox.setText(f'{Text.USE_CALENDAR} {text_range[dropdown_index]}')
+
+        using_calendar_range = self.ui.use_calendar_checkbox.isChecked()
+        if (dropdown_index == RangeType.WEEK or dropdown_index == RangeType.TWO_WEEKS) and using_calendar_range:
+            self.ui.week_start_dropdown.show()
+            self.ui.week_start_label.show()
+        else:
+            self.ui.week_start_dropdown.hide()
+            self.ui.week_start_label.hide()
+
+    def _redraw_cal_checkbox(self):
+        width = self.ui.use_calendar_checkbox.width()
+        height = self.ui.use_calendar_checkbox.height()
+        x = self.ui.use_calendar_checkbox.pos().x()
+        y = self.ui.use_calendar_checkbox.pos().y()
+
+        self.ui.use_calendar_checkbox.setGeometry(QRect(x, y, width, height))
+
+    def _get_excluded_dids(self):
+        names = [self.ui.excluded_decks_list.item(i).text() for i in range(self.ui.excluded_decks_list.count())]
+        return [self.manager.decks.id(item, create=False) for item in names]
+
+    def _load(self):
+        self.ui.week_start_dropdown.setCurrentIndex(self.config[Config.WEEK_START])
+
+        self.ui.range_select_dropdown.setCurrentIndex(self.config[Config.RANGE_TYPE])
+        self.on_range_type_change(self.ui.range_select_dropdown.currentIndex())
+
+        self.ui.use_calendar_checkbox.setChecked(self.config[Config.USE_CALENDAR_RANGE])
+        self.update_calendar_range_extras()
+
+        self.ui.custom_range_spinbox.setValue(self.config[Config.CUSTOM_RANGE])
+        self.ui.total_line.setText(self.config[Config.CUSTOM_TOTAL_TEXT])
+        self.ui.ranged_line.setText(self.config[Config.CUSTOM_RANGE_TEXT])
+
+        # Color Pickers
+        set_background_color(self.ui.primary_color_preview, self.config[Config.PRIMARY_COLOR])
+        set_background_color(self.ui.secondary_color_preview, self.config[Config.SECONDARY_COLOR])
+
+        self.ui.browser_checkbox.setChecked(self.config[Config.BROWSER_ENABLED])
+        self.ui.overview_checkbox.setChecked(self.config[Config.OVERVIEW_ENABLED])
+        self.ui.congrats_checkbox.setChecked(self.config[Config.CONGRATS_ENABLED])
+
+        # Excluded Decks
+        self.excluded_deck_names = [self.manager.decks.name(i) for i in self.config.get(Config.EXCLUDED_DIDS)]
+        self.ui.excluded_decks_list.clear()
+        self.ui.excluded_decks_list.addItems(self.excluded_deck_names)
 
     def _save(self):
         self.config[Config.WEEK_START] = self.ui.week_start_dropdown.currentIndex()
@@ -185,7 +186,3 @@ class TimeStatsOptionsDialog(QDialog):
 
         self.manager.write_config()
         print(f'Result Config: {self.config}')
-
-    def _get_excluded_dids(self):
-        names = [self.ui.excluded_decks_list.item(i).text() for i in range(self.ui.excluded_decks_list.count())]
-        return [self.manager.decks.id(item, create=False) for item in names]
