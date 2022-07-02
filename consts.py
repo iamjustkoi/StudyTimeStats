b6d673ae49d819399513d8df9fc0758174ea987e
iamjustkoi <iamjustkoi@gmail.com>
2022-07-02 08:23:15 -0400

    Changed table element to use max-width property for wide monitor compatibility. Should resolve #2.

--- consts.py
+++ consts.py
@@ -4,6 +4,7 @@ Full license text available in "LICENSE" file, located in the add-on's root dire
 """
 
 CURRENT_VERSION = '1.1.0'
+
 UNIQUE_DATE = '2006-10-05'
 CMD_RANGE = '%range'
 CMD_DATE = '%from_date'
