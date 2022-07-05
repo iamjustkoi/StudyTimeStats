c77cd5f1a102ca4cadb48b4498c39af04a98fc85
iamjustkoi <iamjustkoi@gmail.com>
2022-07-05 12:06:01 -0400

    Added stat toggle options and an icon for the options_dialog

--- consts.py
+++ consts.py
@@ -13,7 +13,7 @@ CMD_YEAR = '%from_year'
 CMD_FULL_DAY = '%from_full_day'
 CMD_DAY = '%from_day'
 CMD_DAYS = '%days'
-ICON_PATH = 'raw/'
+ICON_PATH = 'raw\\icon.svg'
 KOFI_FILEPATH = 'raw\\kofilogo_blue.PNG'
 PATREON_FILEPATH = 'raw\\patreon.png'
 PATREON_URL = 'https://www.patreon.com/iamjustkoi'
@@ -55,6 +55,8 @@ class Config:
     CUSTOM_RANGE_TEXT = 'Custom_Range_Text'
     CUSTOM_HRS_TEXT = 'Custom_Hrs_Text'
     CUSTOM_MIN_TEXT = 'Custom_Min_Text'
+    SHOW_TOTAL = 'Hide_Total_Stat'
+    SHOW_RANGED = 'Hide_Ranged_Stat'
     PRIMARY_COLOR = 'Primary_Color'
     SECONDARY_COLOR = 'Secondary_Color'
     BROWSER_ENABLED = 'Browser_Enabled'
@@ -71,6 +73,8 @@ class Config:
         CUSTOM_RANGE_TEXT: String.PAST_RANGE,
         CUSTOM_HRS_TEXT: String.HRS,
         CUSTOM_MIN_TEXT: String.MIN,
+        SHOW_TOTAL: True,
+        SHOW_RANGED: True,
         PRIMARY_COLOR: 'white',
         SECONDARY_COLOR: '#76bfb4',
         BROWSER_ENABLED: True,
