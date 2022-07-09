36ef4585d1498cd762f3f6c6d71ea5872e69a47e
iamjustkoi <iamjustkoi@gmail.com>
2022-07-09 10:51:08 -0400

    Fixed some issues with bad naming scheme

--- __init__.py
+++ __init__.py
@@ -238,18 +238,18 @@ Currently, uses the string identifiers: %range, %from_date, %from_year, %from_fu
         ref_days_ago = get_days_ago(datetime.today(), range_type, addon_config)
         ref_date = datetime.today() - timedelta(days=ref_days_ago + 1)
         ranged_hrs = get_hrs_in_revlog(get_logs_in_range(revlog, Range.DAYS_IN[range_type], ref_date))
-        unit = addon_config[get_unit_type(ranged_hrs)]
-        disp_hrs = get_hrs_or_min(ranged_hrs)
-        html_string = html_string.replace(CMD_LAST_CAL_HRS, f'{disp_hrs} {unit}')
+        cal_val = get_hrs_or_min(ranged_hrs)
+        cal_unit = addon_config[get_unit_type(ranged_hrs)]
+        html_string = html_string.replace(CMD_LAST_CAL_HRS, f'{cal_val} {cal_unit}')
 
     # Use for not returning duplicates in double-passed variables:
     # match = re.search(fr'(?<!%){CMD_LAST_DAY}', html_string)
     # len(match.regs)
     if re.search(fr'(?<!%){CMD_LAST_DAY_HRS}', html_string):
         ranged_hrs = get_hrs_in_revlog(get_logs_in_range(revlog, days=1, from_date=datetime.today() - timedelta(1)))
-        unit = addon_config[get_unit_type(ranged_hrs)]
-        disp_hrs = get_hrs_or_min(ranged_hrs)
-        html_string = html_string.replace(CMD_LAST_CAL_HRS, f'{disp_hrs} {unit}')
+        day_val = get_hrs_or_min(ranged_hrs)
+        day_unit = addon_config[get_unit_type(ranged_hrs)]
+        html_string = html_string.replace(CMD_LAST_DAY_HRS, f'{day_val} {day_unit}')
 
     if re.search(fr'(?<!%){CMD_RANGE}', html_string):
         if range_type != Range.CUSTOM:
