79695adb81bf85ab19e97c6c90593ba8202c77c1
iamjustkoi <iamjustkoi@gmail.com>
2022-07-11 08:14:28 -0400

    Version update

--- consts.py
+++ consts.py
@@ -3,7 +3,7 @@ MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjust
 Full license text available in the "LICENSE" file, packaged with the add-on.
 """
 
-CURRENT_VERSION = '1.2.10'
+CURRENT_VERSION = '1.2.11'
 
 ANKI_LEGACY_VER = 35
 ANKI_DEFAULT_ROLLOVER = 4
