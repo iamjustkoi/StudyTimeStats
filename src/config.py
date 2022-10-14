4f9d0391b85190eb5c359dc030f0d6f5b6738f5b
iamjustkoi <iamjustkoi@gmail.com>
2022-10-14 13:48:32 -0400

    Added basic reformatting for updating previous configs

--- src/config.py
+++ src/config.py
@@ -2,13 +2,26 @@
 MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
 Full license text available in "LICENSE" file, located in the add-on's root directory.
 """
+import re
+from typing import Any
 
 from anki import buildinfo
 from aqt import AnkiQt
 
-from .consts import Config
+from .consts import CMD_FROM_DATE_HRS, Config
 
 ANKI_VERSION = int(buildinfo.version.replace('2.1.', ''))
+
+
+def _reformat_conf(config: dict[str: Any]):
+    for field in config:
+        data = config[field]
+        if isinstance(data, str):
+            if re.match(fr'.*(?<!%)%from_custom_date:.*', data):
+                config[field] = data.replace('%from_custom_date:', CMD_FROM_DATE_HRS)
+        elif isinstance(data, dict):
+            _reformat_conf(data)
+    return config
 
 
 class TimeStatsConfigManager:
@@ -26,6 +39,7 @@ Generic config manager for accessing and writing addon config values.
         self._meta = self.mw.addonManager.addonMeta(self._addon)
 
         self.config = self._meta.get('config', Config.DEFAULT_CONFIG)
+        self.config = _reformat_conf(self.config)
         self.decks = self.mw.col.decks if self.mw.col is not None else None
         self.max_range = max_filter_range
 
