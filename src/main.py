2d65f8e4541167b28a7e5c894ed73ceb01303e43
iamjustkoi <iamjustkoi@gmail.com>
2022-10-16 17:30:28 -0400

    Added header back to main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+"""
+MIT License: Copyright (c) 2022 JustKoi (iamjustkoi) <https://github.com/iamjustkoi>
+Full license text available in the "LICENSE" file, packaged with the add-on.
+"""
+
 import re
 import traceback
 from datetime import date, datetime, timedelta
