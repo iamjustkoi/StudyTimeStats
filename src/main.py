3876209ecdf0298cd54de6471d196916dfb01ad0
iamjustkoi <iamjustkoi@gmail.com>
2023-02-02 12:18:16 -0500

    Commented out some migrated code

--- src/main.py
+++ src/main.py
@@ -46,41 +46,41 @@ from .consts import (
 from .options import TimeStatsOptionsDialog
 
 
-def build_actions():
-    """
-    Add and connect addon actions. Currently, adds an options menu action and sets the addon configuration action,
-    found in Anki's addon window, to also open the options menu.
-    """
-    refresh_tools_menu_action()
-    mw.addonManager.setConfigAction(__name__, on_options_called)
-
-
-def refresh_tools_menu_action(changes=None, obj=None):
-    """
-    Updates the toolbar actions menu with the options shortcut. Expects an Operation Change hook call,
-    but can also be used as a general update push, too.
-
-    :param changes: unused OpChanges object
-    :param obj: unused options object
-    """
-    if get_config_manager().config[Config.TOOLBAR_ENABLED]:
-        options_action = QAction(String.OPTIONS_ACTION, mw)
-        options_action.triggered.connect(on_options_called)
-        # Handles edge cases where toolbar action already exists in the tools menu
-        if options_action.text() not in [action.text() for action in mw.form.menuTools.actions()]:
-            mw.form.menuTools.addAction(options_action)
-    else:
-        for action in mw.form.menuTools.actions():
-            if action.text() == String.OPTIONS_ACTION:
-                mw.form.menuTools.removeAction(action)
-
-
-def on_options_called():
-    """
-    Initializes and opens the options dialog.
-    """
-    dialog = TimeStatsOptionsDialog(get_config_manager())
-    dialog.exec()
+# def build_actions():
+#     """
+#     Add and connect addon actions. Currently, adds an options menu action and sets the addon configuration action,
+#     found in Anki's addon window, to also open the options menu.
+#     """
+#     refresh_tools_menu_action()
+#     mw.addonManager.setConfigAction(__name__, on_options_called)
+#
+#
+# def refresh_tools_menu_action(changes=None, obj=None):
+#     """
+#     Updates the toolbar actions menu with the options shortcut. Expects an Operation Change hook call,
+#     but can also be used as a general update push, too.
+#
+#     :param changes: unused OpChanges object
+#     :param obj: unused options object
+#     """
+#     if get_config_manager().config[Config.TOOLBAR_ENABLED]:
+#         options_action = QAction(String.OPTIONS_ACTION, mw)
+#         options_action.triggered.connect(on_options_called)
+#         # Handles edge cases where toolbar action already exists in the tools menu
+#         if options_action.text() not in [action.text() for action in mw.form.menuTools.actions()]:
+#             mw.form.menuTools.addAction(options_action)
+#     else:
+#         for action in mw.form.menuTools.actions():
+#             if action.text() == String.OPTIONS_ACTION:
+#                 mw.form.menuTools.removeAction(action)
+#
+#
+# def on_options_called():
+#     """
+#     Initializes and opens the options dialog.
+#     """
+#     dialog = TimeStatsOptionsDialog(get_config_manager())
+#     dialog.exec()
 
 
 def get_config_manager() -> TimeStatsConfigManager:
@@ -494,6 +494,7 @@ def build_hooks():
     gui_hooks.overview_will_render_content.append(append_to_overview)
     if ANKI_VERSION > ANKI_LEGACY_VER:
         gui_hooks.webview_did_inject_style_into_page.append(append_to_congrats)
-        gui_hooks.operation_did_execute.append(refresh_tools_menu_action)
+        # gui_hooks.operation_did_execute.append(refresh_tools_menu_action)
     else:
-        gui_hooks.state_did_reset.append(refresh_tools_menu_action)
+        # gui_hooks.state_did_reset.append(refresh_tools_menu_action)
+        pass
