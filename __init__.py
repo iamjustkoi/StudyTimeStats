23a4d808ff15a3911b09d20b63d583b2e1d8bc9a
iamjustkoi <iamjustkoi@gmail.com>
2023-02-02 12:54:51 -0500

    Removed main imports and implemented primary overview.py calls

--- __init__.py
+++ __init__.py
@@ -4,8 +4,7 @@
 """
 Shows total study time and a ranged amount of study time in Anki's main window.
 """
-
-from .src import main
+from .src import overview
 from .src import toolbar
 
 
@@ -13,8 +12,7 @@ def initialize():
     """
 Initializer for the add-on. Called at the start for finer execution order and a bit of readability.
     """
-    # main.build_hooks()
-    # main.build_actions()
+    overview.build_hooks()
     toolbar.build_toolbar_actions()
 
 
