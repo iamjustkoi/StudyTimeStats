1ac212d2c621ee9232f821edc08945376d1796d2
iamjustkoi <iamjustkoi@gmail.com>
2023-04-18 13:11:16 -0400

    add version 2.0.0 config patch

--- src/config.py
+++ src/config.py
@@ -17,13 +17,68 @@ def _reformat_conf(config: dict):
     :param config: The current config for the addon.
     :return: An updated addon config with some redone formatting.
     """
-    for field in config:
-        data = config[field]
-        if isinstance(data, str):
-            if re.match(fr'.*(?<!%)%from_custom_date:.*', data):
-                config[field] = data.replace('%from_custom_date:', Macro.CMD_FROM_DATE_HOURS)
-        elif isinstance(data, dict):
-            _reformat_conf(data)
+    addon_ver = config.get(Config.VERSION, '0.0.0')
+    formatted_addon_ver = re.sub('-.*', '', addon_ver)
+    ver_numbers: list[int] = [int(n) for n in formatted_addon_ver.split('.')]
+
+    # v1.3.5
+    if ver_numbers[0] <= 1 and ver_numbers[1] <= 3 and ver_numbers[2] <= 5:
+        print(f'run_replace')
+
+        def replace_macro(outer_conf: dict):
+            for field in outer_conf:
+                data = outer_conf[field]
+                if isinstance(data, str) and re.match(fr'.*(?<!%)%from_custom_date:.*', data):
+                    outer_conf[field] = data.replace('%from_custom_date:', Macro.CMD_FROM_DATE_HOURS)
+                elif isinstance(data, dict):
+                    replace_macro(data)
+
+        replace_macro(config)
+
+    # v2.0.0
+    if ver_numbers[0] < 2 and not config.get(Config.CELLS_DATA, None):
+        print(f'run_cave_exit')
+        cells = []
+        default_data = Config.DEFAULT_CELL_DATA.copy()
+        if not config.get('Hide_Total_Stat', False):
+            cells.append(
+                {
+                    Config.TITLE: config.get('Custom_Total_Text', String.TOTAL),
+                    Config.OUTPUT: config.get('Custom_Total_Hrs', Macro.CMD_RANGE_HOURS),
+                    Config.TITLE_COLOR: config.get('Primary_Color', default_data[Config.TITLE_COLOR]),
+                    Config.OUTPUT_COLOR: config.get('Secondary_Color', default_data[Config.OUTPUT_COLOR]),
+                    Config.DIRECTION: Direction.VERTICAL,
+                    Config.RANGE: Range.TOTAL,
+                    Config.USE_CALENDAR: default_data[Config.USE_CALENDAR],
+                    Config.WEEK_START: default_data[Config.WEEK_START],
+                    Config.DAYS: default_data[Config.DAYS],
+                    Config.HRS_UNIT: config.get('Custom_Hrs_Text', default_data[Config.HRS_UNIT]),
+                    Config.MIN_UNIT: config.get('Custom_Min_Text', default_data[Config.MIN_UNIT]),
+                    Config.HTML: CELL_HTML_SHELL,
+                }
+            )
+        if not config.get('Hide_Ranged_Stat', False):
+            cells.append(
+                {
+                    Config.TITLE: config.get('Custom_Range_Text', String.PAST_RANGE),
+                    Config.OUTPUT: config.get('Custom_Range_Hrs', Macro.CMD_RANGE_HOURS),
+                    Config.TITLE_COLOR: config.get('Primary_Color', default_data[Config.TITLE_COLOR]),
+                    Config.OUTPUT_COLOR: config.get('Secondary_Color', default_data[Config.OUTPUT_COLOR]),
+                    Config.DIRECTION: Direction.VERTICAL,
+                    Config.RANGE: config.get('Range_Type', default_data[Config.RANGE]),
+                    Config.USE_CALENDAR: config.get('Use_Calendar_Range', default_data[Config.USE_CALENDAR]),
+                    Config.WEEK_START: config.get('Week_Start', default_data[Config.WEEK_START]),
+                    Config.DAYS: config.get('Custom_Days', default_data[Config.DAYS]),
+                    Config.HRS_UNIT: config.get('Custom_Hrs_Text', default_data[Config.HRS_UNIT]),
+                    Config.MIN_UNIT: config.get('Custom_Min_Text', default_data[Config.MIN_UNIT]),
+                    Config.HTML: CELL_HTML_SHELL,
+                }
+            )
+
+        config[Config.CELLS_DATA] = cells
+
+    config[Config.VERSION] = CURRENT_VERSION
+
     return config
 
 
@@ -41,13 +96,6 @@ class TimeStatsConfigManager:
         self._meta = self.mw.addonManager.addonMeta(self._addon)
 
         self.config = self._init_config()
-
-        # self.config = self._meta.get('config', Config.DEFAULT_CONFIG)
-        # self.config = _reformat_conf(self.config)
-        # for field in Config.DEFAULT_CONFIG:
-        #     if field not in self.config:
-        #         # load temp defaults
-        #         self.config[field] = Config.DEFAULT_CONFIG[field]
 
         self.decks = self.mw.col.decks if self.mw.col is not None else None
 
